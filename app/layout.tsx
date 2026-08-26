23c4cec5c8df872a67588bd5ed0fe3f3a00a860b
OPNX LLC <kdhogg@opnx-talent.com>
2026-08-26 04:20:19 -0400

    Add global bottom clearance stylesheet

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import './matrix.css';
 import './candidate-interview-scroll.css';
+import './bottom-clearance.css';
 import { cookies, headers } from 'next/headers';
 import { AppShell } from '@/components/AppShell';
 import { supabaseAdmin } from '@/lib/supabaseAdmin';
